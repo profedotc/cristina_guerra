b1b0e6da822167b05e25d6ba056bfbf3d09bb969
Cristina Guerra <cristina.guerub@gmail.com>
2019-06-03 19:19:21 +0200

    Ejercicio 3.1
    Signed-off-by: Cristina Guerra <cristina.guerub@gmail.com>

--- gol.h
+++ gol.h
@@ -6,9 +6,14 @@
 #define TAM_X 10
 #define TAM_Y 10
 
-void gol_init(bool mundo[TAM_X][TAM_Y]);
-void gol_print(bool mundo[TAM_X][TAM_Y]);
-void gol_step(bool mundo0[TAM_X][TAM_Y], bool mundo1[TAM_X][TAM_Y]);
+struct gol {
+	bool worlds[2][TAM_X][TAM_Y];
+	int current_world;
+};
+
+void gol_init(struct gol *gol);
+void gol_print(struct gol *gol);
+void gol_step(struct gol *gol);
 int gol_count_neighbors(bool mundo[TAM_X][TAM_Y], bool *position);
 bool gol_get_cell(bool mundo[TAM_X][TAM_Y], bool *position);
 #endif
